f1fd39942c91dedd08efafc69ecc236a5c388047
Rubenval <rnmjamsoft@gmail.com>
2021-04-23 17:38:14 -0300

    Add

---
+++ exrcFixacao/exrcFixacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exrcFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
